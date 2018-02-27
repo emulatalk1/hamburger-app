7f9730b7f21e18acd52a2dbbf3786fa99b3d76ce
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2018-02-27 14:12:39 +0700

    #134 - Adding a Hamburger menu icon

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,6 @@ import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <DrawerToggle clicked={props.drawerToggleClicked}/>
-        <div>MENU</div>
         <div className={classes.Logo}>
             <Logo />
         </div>
