099c9a82c29bfec9f7851c6e79d01f2df8e85ce8
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2018-03-03 17:21:32 +0700

    #219 - Dynamically create inputs vased on js config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -81,12 +81,22 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementArray = [];
+        for (let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (
             <form>
-                <Input elementType="..." elementConfig="..." value="..." />
-                <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
-                <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal code" />
+                {formElementArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value} />
+                ))}
                 <Button buttonType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
