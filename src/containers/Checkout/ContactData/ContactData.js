70f47da0ca7e2bf3c0c41cb45554c34bc7de771f
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2018-03-04 03:07:03 +0700

    fixed null value at select box data form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,7 +84,8 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
-                value: '',
+                value: 'fastest',
+                validation: {},
                 valid: true
             }
         },
@@ -116,6 +117,10 @@ class ContactData extends Component {
 
     checkValidity(value, rules) {
         let isValid = true;
+
+        if (!rules) {
+            return;
+        }
 
         if(rules.required) {
             isValid = value.trim() !== '' && isValid;
