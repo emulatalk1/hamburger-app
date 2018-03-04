61762251163fc3ab66fe3a9a4c680f64b87bfaf2
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2018-03-04 17:38:37 +0700

    #256 - Finishing the Reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,13 +31,13 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        axios.get('https://hamburgerhieu.firebaseio.com/ingredients.json')
-            .then(res => {
-                this.setState({ingredients: res.data})
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });
+        // axios.get('https://hamburgerhieu.firebaseio.com/ingredients.json')
+        //     .then(res => {
+        //         this.setState({ingredients: res.data})
+        //     })
+        //     .catch(error => {
+        //         this.setState({error: true});
+        //     });
     }
 
     updatePurchaseState (ingredients) {
