1e50a249cb40dc167e6581370d0aaaa63455ba82
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2018-03-06 14:27:20 +0700

    #307 - adding sign-in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -29,7 +29,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA0f0kMVUdZENC1zAX4e0YthFa1Ec66WF8', authData)
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA0f0kMVUdZENC1zAX4e0YthFa1Ec66WF8';
+        if (!isSignup) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyA0f0kMVUdZENC1zAX4e0YthFa1Ec66WF8';
+        }
+        axios.post(url, authData)
             .then(res => {
                 console.log(res)
                 dispatch(authSuccess(res.data));
